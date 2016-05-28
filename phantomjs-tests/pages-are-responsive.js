7028d9d6ff6aa449121a9968e76b96bff0c429de
brownhead <johnsullivan.pem@gmail.com>
2016-05-28 00:04:20 -0700

    Add bind polyfil to phantomjs test.
    Test Plan:
    Switch to a pre-2.x version of phantom js, then run
    
    `make phantomjs-tests`

--- phantomjs-tests/pages-are-responsive.js
+++ phantomjs-tests/pages-are-responsive.js
@@ -23,6 +23,35 @@ VIEWPORT_WIDTH = 568;
 // The results of the testing (each item will be a 2-list with [url, width])
 results = [];
 
+// Bind polyfill from
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(
+                this instanceof fNOP ? this : oThis,
+                aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    if (this.prototype) {
+      // Function.prototype doesn't have a prototype property
+      fNOP.prototype = this.prototype;
+    }
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 // Exit if all the tests have finished
 var maybeExit = function() {
     if (results.length !== system.args.length - 1) {
