e56ad4d5209ec2b60c8bc6af2a30004ccb580253
John Sullivan <johnsullivan.pem@gmail.com>
2017-05-31 11:43:05 -0700

    Add Brian's upcoming post.

--- src/info.py
+++ src/info.py
@@ -178,12 +178,10 @@ authors = {
 # src/styles/post-template.less.  Just remove the 'team-' to get the
 # `team` value, replace dashes with spaces, and capitalize words.
 # See README.rst for more info.
-upcoming_post = None
-'''Leaving this here so we know what shape this wants to be
-{
-    "title": "Migrating to a Mobile Monorepo for React Native",
-    "team": "Mobile",
-    "team_class": "team-mobile",
-    "author": authors["Jared Forsyth"],
-    "published_on_html": "May 29<sup aria-hidden='true'>th</sup>",
-}'''
+upcoming_post = {
+    "title": "Creating Query Components with Apollo",
+    "team": "Web Frontend",
+    "team_class": "team-web-frontend",
+    "author": authors["Brian Genisio"],
+    "published_on_html": "June 12<sup aria-hidden='true'>th</sup>",
+}
